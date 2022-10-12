d2003a4e6a8218702138845b1e0ba23161c08dae
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-10-12 09:53:30 -0700

    Edit routes to pull loader from route params.

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Root, {
   action as rootAction
 } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +19,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: "contacts/:contactId",
-        element: <Contact />
+        element: <Contact />,
+        loader: contactLoader
       }
     ]
   }
